3884207ab7be54433b88fc41858d368dbbe16ffa
shiannedinu <shiannedinu@gmail.com>
2014-11-26 14:25:56 +1100

    Update Arithmetic Progression.java

--- Arithmetic Progression.java
+++ Arithmetic Progression.java
@@ -1,3 +1,25 @@
+/*
+Task: Find the missing term in an Arithmetic Progression.
+
+Input Format
+The first line contains an Integer N, which is the number of terms which will be provided as input.
+This is followed by N consecutive Integers, with a space between each pair of integers. All of these are on one line, and they are in AP (other than the point where an integer is missing).
+ 
+ 
+Output Format
+One Number which is the missing integer from the series.
+ 
+Sample Input
+5
+1 3 5 9 11  
+ 
+Sample Output
+7
+
+*/
+
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
